85856fabeffacb1e7f6067f3a383ce96692f16d0
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-04-16 22:06:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
- function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
+    tecla.onclick = () => {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = (evento) => {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = () => {
+        tecla.classList.remove('ativa');
+    }
+
+}
 
 /*
 const botoes = document.querySelectorAll('.tecla');
